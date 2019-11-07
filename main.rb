7309cac40b817121afadff2f626641dd149ae59f
Aleem Pathammavong <Aleemp321@yahoo.com>
2019-11-07 11:21:19 -0700

    add gemfile and colorized gem

--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
+require "pry"
+require "colorize" 
+
+
 def puts_git(cmd) 
 
   puts `git #{cmd} - h`
@@ -6,20 +10,22 @@ def puts_git(cmd)
 end 
 
 def menu 
-  puts "1: Enter git command"
-  puts "2: Exit"
+  puts "MAIN MENU".colorize(:red) 
+  puts "1: Enter git command".colorize(:blue)
+  puts "2: Exit".colorize(:yellow)
 
   case gets.to_i 
     when 1 
-      puts "Enter git command:" 
+      puts "Enter git command:".colorize(:green)
       puts_git(gets.strip)
       menu 
     when 2
       exit 
     else 
-      puts "Invalid choice"
+      puts "Invalid choice".colorize(:orange)
       menu 
     end 
 end
 # back ticks ` ` runs a command instead of just printing 
+# i to insert esc to go back :wp w(save) q(quit)
 menu 
